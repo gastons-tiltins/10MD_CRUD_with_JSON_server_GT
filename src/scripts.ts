87589c2070e223339397a00e7586f64719bfccf2
Gastons Tiltins <gastons.tiltins@gmail.com>
2023-01-23 12:45:39 +0200

    FUNCTIONALITY WORKS

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,162 +1,184 @@
 import axios from 'axios';
-
-// const allCars = document.querySelector('.car');
-const carImage = document.querySelector('.car-image') as HTMLImageElement;
-const carName = document.querySelector('.car-name');
-const carDesc = document.querySelector('.car-desc');
-
-axios
-    // .get('http://localhost:3004/cars')
-    .get('http://localhost:3004/cars', {
-        responseType: 'json',
-    })
-    .then(function (response) {
-        // id amount
-        let idCount = countId(response);
-
-        for (let i = 0; i < idCount; i++) {
-            let id = response.data[i].id;
-            let img = response.data[i].image;
-            let name = response.data[i].name;
-            let desc = response.data[i].desc;
-
-            let card = document.createElement('card');
-            card.setAttribute('id', id);
-            card.innerHTML = `<div class="car column">
-                <img class="car-image" src="${img}" alt="car" />
-                <div class="car-name">${name}</div>
-                <div class="car-desc">${desc}</div>
-                <button type="button" class="editButton${id} confirm-add-car btn btn-outline-secondary">Edit</button>
-                <button type="button" class="deleteButton${id} confirm-add-car btn btn-outline-secondary">Delete</button>
-            </div>`;
-            document.getElementsByClassName('all-cars')[0].appendChild(card);
-            // -----------------------------------
-            // HANDLE BUTTONS
-            // -----------------------------------
-            const deletetCarButton = document.querySelector(
-                `.deleteButton${id}`,
-            );
-            deletetCarButton.addEventListener('click', () => {
-                async function deleteShit(id: number) {
-                    await axios.delete(`http://localhost:3004/cars/${id}`);
-                    console.log('deleted');
-                    window.location.reload();
-                }
-                deleteShit(id);
-            });
-        }
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-    .finally(function () {
-        // always executed
-    });
+const url = 'http://localhost:3004/cars';
 
 // -----------------------------------
 // ADD CAR
 // -----------------------------------
-const addCar = document.querySelector('.addButton');
-addCar.addEventListener('click', () => {
-    let showAddForm = document.querySelector('.add-form');
+const addCarButton = document.querySelector('.add-car');
+addCarButton.addEventListener('click', () => {
+    let showAddForm = document.querySelector('.add-car-form');
     showAddForm.classList.remove('hide');
 });
 
-// CANCEL ADD BUTTON
+// -----------------------------------
+// CANCEL ADD FORM BUTTON
+// -----------------------------------
 const cancelCarConfirm = document.querySelector('.cancel-add-car');
 cancelCarConfirm.addEventListener('click', () => {
-    let hideAddForm = document.querySelector('.add-form');
+    let hideAddForm = document.querySelector('.add-car-form');
     hideAddForm.classList.add('hide');
 });
 
-const addCarConfirm = document.querySelector('.confirm-add-car');
-addCarConfirm.addEventListener('click', () => {
-    axios
-        // .get('http://localhost:3004/cars')
-        .get('http://localhost:3004/cars', {
-            responseType: 'json',
-        })
-        .then(function (response) {
-            let id: any;
-            let idArr = idArray(response);
-            for (let i = 0; i < 100; i++) {
-                if (idArr.includes(i)) {
-                    continue;
-                } else {
-                    id = i;
-                    break;
-                }
-            }
-
-            let name = (<any>document.querySelector('.add-car-name')).value;
-            let img = (<any>document.querySelector('.add-card-img')).value;
-            let desc = (<any>document.querySelector('.add-car-desc')).value;
-
-            if (name.length > 0 && img.includes(`http`) && desc.length > 0) {
-                axios.post(`http://localhost:3004/cars/`, {
-                    id: `${id}`,
-                    name: `${name}`,
-                    image: `${img}`,
-                    desc: `${desc}`,
-                });
-            }
-
-            let card = document.createElement('card');
-            card.setAttribute('id', id);
-            card.innerHTML = `<div class="car column">
-                <img class="car-image " src="${img}" alt="car" />
-                <div class="car-name">${name}</div>
-                <div class="car-desc">${desc}</div>
-                <button type="button" class="editButton${id} confirm-add-car btn btn-outline-secondary">Edit</button>
-                <button type="button" class="deleteButton${id} confirm-add-car btn btn-outline-secondary">Delete</button>
-            </div>`;
-
-            // document.getElementsByClassName('all-cars')[0].appendChild(card);
-
-            async function makeCard(card: any) {
-                document
-                    .getElementsByClassName('all-cars')[0]
-                    .appendChild(card);
-                window.location.reload();
-            }
-            makeCard(card);
-
-            // -----------------------------------
-            // HANDLE BUTTONS
-            // -----------------------------------
-            const deletetCarButton = document.querySelector(
-                `.deleteButton${id}`,
-            );
-            deletetCarButton.addEventListener('click', () => {
-                async function deleteCar(id: number) {
-                    await axios.delete(`http://localhost:3004/cars/${id}`);
-                    console.log('deleted');
-                    window.location.reload();
-                }
-                deleteCar(id);
-            });
-        });
+// -----------------------------------
+// CANCEL EDIT FORM BUTTON
+// -----------------------------------
+const cancelEditForm = document.querySelector('.cancel-edit-car');
+cancelEditForm.addEventListener('click', () => {
+    let hideAddForm = document.querySelector('.edit-car-form');
+    hideAddForm.classList.add('hide');
 });
 
-// Id count from JSON
-const countId = (data: any) => {
-    let idCount = 0;
-    data.data.map(function (val: any) {
-        if (val.id) {
-            idCount++;
-        }
-    });
-    return idCount;
-};
+// Make card with data
+function makeCard(data: any, i: number) {
+    let id = data[i].id;
+    let name = data[i].name;
+    let image = data[i].image;
+    let desc = data[i].desc;
 
-// Id Array
+    let card = document.createElement('card');
+    card.innerHTML = `
+    <div class="car" id="${id}">
+        <img class="car-image" src="${image}" alt="${name}" />
+        <div class="name">${name}</div>
+        <div class="desc">${desc}</div>
+        <button type="button" id="editButton${id}" class="btn btn-outline-secondary">Edit</button>
+        <button type="button" id="deleteButton${id}" class="btn btn-outline-secondary">Delete</button>
+        <hr>
+    </div>
+    `;
+    document.getElementsByClassName('all-cars')[0].appendChild(card);
+}
+
+// JSON read ALL
+async function readAllJson() {
+    let response = await axios.get(url);
+    let data = response.data;
+    // console.log(data[0].name);
+    return data;
+}
+
+// Create all cars
+async function outputCars() {
+    let data = await readAllJson();
+    let idCount = Object.keys(data).length;
+    for (let i = 0; i < idCount; i++) {
+        makeCard(data, i);
+    }
+    activateButtons();
+}
+outputCars();
+
+async function activateButtons() {
+    const buttons = document.getElementsByTagName('button');
+    const result = document.getElementById('result');
+
+    const buttonPressed = (e: any) => {
+        let button = e.target.id;
+        if (button.includes('delete')) {
+            deleteItem(button);
+        }
+        if (button.includes('edit')) {
+            let hideAddForm = document.querySelector('.edit-car-form');
+            hideAddForm.classList.remove('hide');
+            editItem(button);
+        }
+    };
+
+    for (let button of buttons) {
+        button.addEventListener('click', buttonPressed);
+    }
+}
+
+async function deleteItem(button: string) {
+    let id = Number(button.replace('deleteButton', ''));
+    await axios.delete(`${url}/${id}`);
+    window.location.reload();
+}
+
+// Add car
+async function addCar() {
+    let data = await readAllJson();
+    let id = idArray(data);
+    let name = (<any>document.querySelector('.add-car-name')).value;
+    let image = (<any>document.querySelector('.add-card-image')).value;
+    let desc = (<any>document.querySelector('.add-car-desc')).value;
+    console.log(id, name, image, desc);
+    if (name.length > 0 && image.includes(`http`) && desc.length > 0) {
+        await axios
+            .post(`http://localhost:3004/cars/`, {
+                id: `${id}`,
+                name: `${name}`,
+                image: `${image}`,
+                desc: `${desc}`,
+            })
+            .then(function (response) {
+                window.location.reload();
+            });
+    } else {
+        document.getElementById('add-error').innerHTML =
+            'Incorect input! Try again.';
+    }
+}
+
+const addCarConfirm = document.querySelector('.confirm-add-car');
+addCarConfirm.addEventListener('click', () => {
+    addCar();
+});
+
+// Return free JSON ID
 const idArray = (data: any) => {
     let idArray: any = [];
-    data.data.map(function (val: any) {
+    let id: number;
+    data.map(function (val: any) {
         if (val.id) {
             idArray.push(Number(val.id));
         }
     });
-    return idArray;
+    for (let i = 0; i < 100; i++) {
+        if (idArray.includes(i)) {
+            continue;
+        } else {
+            id = i;
+            return id;
+        }
+    }
 };
+
+// Edit item
+async function editItem(button: string) {
+    let data = await readAllJson();
+    let id = Number(button.replace('editButton', ''));
+    let name = data[id].name;
+    let image = data[id].image;
+    let desc = data[id].desc;
+    (document.querySelector('.edit-car-name') as HTMLInputElement).value = name;
+    (document.querySelector('.edit-card-image') as HTMLInputElement).value =
+        image;
+    (document.querySelector('.edit-car-desc') as HTMLInputElement).value = desc;
+
+    const EditCarConfirm = document.querySelector('.confirm-edit-car');
+    EditCarConfirm.addEventListener('click', () => {
+        let setName = (<any>document.querySelector('.edit-car-name')).value;
+        let setImage = (<any>document.querySelector('.edit-card-image')).value;
+        let setDesc = (<any>document.querySelector('.edit-car-desc')).value;
+        editCar(id, setName, setImage, setDesc);
+    });
+}
+
+async function editCar(id: number, name: string, image: string, desc: string) {
+    if (name.length > 0 && image.includes(`http`) && desc.length > 0) {
+        await axios
+            .patch(`http://localhost:3004/cars/${id}`, {
+                id: `${id}`,
+                name: `${name}`,
+                image: `${image}`,
+                desc: `${desc}`,
+            })
+            .then(function (response) {
+                window.location.reload();
+            });
+    } else {
+        document.getElementById('edit-error').innerHTML =
+            'Incorect input! Try again.';
+    }
+}
